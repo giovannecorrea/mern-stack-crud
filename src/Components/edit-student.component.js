9d39ca05cd772ef784fe68dbabf5396152451658
Giovanne Corrêa <giovannecorrea@gmail.com>
2022-08-14 18:42:27 -0300

    react router fixes

--- src/Components/edit-student.component.js
+++ src/Components/edit-student.component.js
@@ -3,57 +3,63 @@
 // Import Modules
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
 import StudentForm from "./StudentForm";
 
 // EditStudent Component
 const EditStudent = (props) => {
-const [formValues, setFormValues] = useState({
-	name: "",
-	email: "",
-	rollno: "",
-});
-	
-//onSubmit handler
-const onSubmit = (studentObject) => {
-	axios
-	.put(
-		"http://localhost:4000/students/update-student/" +
-		props.match.params.id,
-		studentObject
-	)
-	.then((res) => {
-		if (res.status === 200) {
-		alert("Student successfully updated");
-		props.history.push("/student-list");
-		} else Promise.reject();
-	})
-	.catch((err) => alert("Something went wrong"));
-};
+	const [formValues, setFormValues] = useState({
+		name: "",
+		email: "",
+		rollno: "",
+	});
+	const student_id = useParams();
+	const navigate = useNavigate();
+		
+	//onSubmit handler
+	const onSubmit = (studentObject) => {
+		axios
+		.put(
+			"http://localhost:4000/students/update-student/" +
+			student_id.id,
+			studentObject
+		)
+		.then((res) => {
+			if (res.status === 200) {
+			alert("Student successfully updated");
+			navigate("/student-list");
+			} else Promise.reject();
+		})
+		.catch((err) => {
+				alert("Something went wrong");
+				console.log(err);
+		});
+	};
 
-// Load data from server and reinitialize student form
-useEffect(() => {
-	axios
-	.get(
-		"http://localhost:4000/students/update-student/"
-		+ props.match.params.id
-	)
-	.then((res) => {
-		const { name, email, rollno } = res.data;
-		setFormValues({ name, email, rollno });
-	})
-	.catch((err) => console.log(err));
-}, []);
+	// Load data from server and reinitialize student form
+	useEffect(() => {
+		axios
+		.get(
+			"http://localhost:4000/students/update-student/"
+			+ student_id.id
+		)
+		.then((res) => {
+			const { name, email, rollno } = res.data;
+			setFormValues({ name, email, rollno });
+		})
+		.catch((err) => console.log(err));
+	}, []);
 
-// Return student form
-return (
-	<StudentForm
-	initialValues={formValues}
-	onSubmit={onSubmit}
-	enableReinitialize
-	>
-	Update Student
-	</StudentForm>
-);
+	// Return student form
+	return (
+		<StudentForm
+		initialValues={formValues}
+		onSubmit={onSubmit}
+		enableReinitialize
+		>
+		Update Student
+		</StudentForm>
+	);
 };
 
 // Export EditStudent Component
